3f30fba235d40d091e1c4a100799797ecf33683f
Julian Wong <jwong01@stuy.edu>
2019-02-10 21:10:59 -0500

    start rewriting sh() because i messed up before

--- QueenBoard.java
+++ QueenBoard.java
@@ -20,38 +20,23 @@ public class QueenBoard {
     return sh(0, 0);
   }
   public boolean sh(int r, int c) {
-    for(int q = 0; q < board.length; q++) {
-      for(int w = 0; w < board[q].length; w++) {
-        if(board[q][w] == -1) {
-          if(w + 1 == board[q].length) {
-            q--;
-            for(int e = 0; e < board[q].length; e++) {
-              if(board[q][e] == -1) {
-                w = e;
-                rq(q, w);
-                return sh(q, w + 1);
-              }
-            }
+    if(r >= board.length) {
+      for(int q = 0; q < board.length; q++) {
+        for(int w = 0; w < board[q].length; w++) {
+          if(board[q][w] == -1) {
+            break;
           }
-          else {
-            return sh(q, w + 1);
-          }
-        }
-        else {
-          aq(q, w);
-          break;
+          return false;
         }
       }
     }
-    System.out.println(board.toString());
-    return true;
   }
-  public void deathRays(int[][] board, int r, int c, int aor) {
+  public void deathRaysAdd(int[][] board, int r, int c) {
     //bottom right
     int qr1 = r;
     int qc1 = c;
     while(qc1 < board[r].length && qr1 < board.length) {
-      board[qr1][qc1] = aor;
+      board[qr1][qc1]++;
       qr1++;
       qc1++;
     }
@@ -59,26 +44,49 @@ public class QueenBoard {
     int qr2 = r;
     int qc2 = c;
     while(qc2 >= 0 && qr2 < board.length) {
-      board[qr2][qc2] = aor;
+      board[qr2][qc2]++;
       qr2++;
       qc2--;
     }
     //down
     for(int qr3 = r; qr3 < board.length; qr3++) {
-      board[qr3][c] = aor;
+      board[qr3][c]++;
+    }
+  }
+  public void deathRaysDel(int[][] board, int r, int c) {
+    //bottom right
+    int qr1 = r;
+    int qc1 = c;
+    while(qc1 < board[r].length && qr1 < board.length) {
+      board[qr1][qc1]--;
+      qr1++;
+      qc1++;
+    }
+    //bottom left
+    int qr2 = r;
+    int qc2 = c;
+    while(qc2 >= 0 && qr2 < board.length) {
+      board[qr2][qc2]--;
+      qr2++;
+      qc2--;
+    }
+    //down
+    for(int qr3 = r; qr3 < board.length; qr3++) {
+      board[qr3][c]--;
     }
   }
   private boolean rq(int r, int c) {
+    deathRaysDel(board, r, c);
     board[r][c] = 0;
-    deathRays(board, r, c, 0);
     return true;
   }
   private boolean aq(int r, int c) {
+    deathRaysAdd(board, r, c, 1);
     board[r][c] = -1;
-    deathRays(board, r, c, -1);
     return true;
   }
   public static void main(String[] args) {
-
+    QueenBoard test = new QueenBoard(4);
+    test.solve();
   }
 }
